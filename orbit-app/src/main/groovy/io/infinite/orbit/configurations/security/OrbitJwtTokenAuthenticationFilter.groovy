b13655509e727db40c41dd44888581338dad8c1c
apryamostanov <anton.pryamostanov@gmail.com>
2020-04-23 23:10:09 +0300

    Progress

--- orbit-app/src/main/groovy/io/infinite/orbit/configurations/security/OrbitJwtTokenAuthenticationFilter.groovy
+++ orbit-app/src/main/groovy/io/infinite/orbit/configurations/security/OrbitJwtTokenAuthenticationFilter.groovy
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service
 import org.springframework.web.filter.OncePerRequestFilter
 
 import javax.servlet.FilterChain
-import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
