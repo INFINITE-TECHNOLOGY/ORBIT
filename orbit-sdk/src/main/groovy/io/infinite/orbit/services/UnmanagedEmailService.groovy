ef6f0ac20084b7bd265b7b4c0b9028d8bd532feb
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-09 06:12:06 +0300

    Progress

--- orbit-sdk/src/main/groovy/io/infinite/orbit/services/UnmanagedEmailService.groovy
+++ orbit-sdk/src/main/groovy/io/infinite/orbit/services/UnmanagedEmailService.groovy
@@ -1,14 +1,11 @@
 package io.infinite.orbit.services
 
+import com.sendgrid.*
 import groovy.util.logging.Slf4j
 import io.infinite.blackbox.BlackBox
 import io.infinite.carburetor.CarburetorLevel
 import io.infinite.orbit.model.UnmanagedEmail
-import org.springframework.mail.javamail.JavaMailSenderImpl
-import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
-
-import javax.mail.internet.MimeMessage
 
 @Service
 @BlackBox(level = CarburetorLevel.METHOD)
@@ -16,26 +13,20 @@ import javax.mail.internet.MimeMessage
 class UnmanagedEmailService {
 
     void unmanagedEmail(UnmanagedEmail unmanagedEmail) {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl()
-        mailSender.setHost("smtp.gmail.com")
-        mailSender.setPort(465)
-        mailSender.setUsername(System.getenv("GMAIL_USERNAME"))
-        mailSender.setPassword(System.getenv("GMAIL_PASSWORD"))
-        Properties mailProp = mailSender.getJavaMailProperties()
-        mailProp.put("mail.transport.protocol", "smtp")
-        mailProp.put("mail.smtp.auth", "true")
-        mailProp.put("mail.smtp.starttls.enable", "true")
-        mailProp.put("mail.smtp.starttls.required", "true")
-        mailProp.put("mail.debug", "true")
-        mailProp.put("mail.smtp.ssl.enable", "true")
-        mailProp.put("mail.smtp.user", System.getenv("GMAIL_USERNAME"))
-        MimeMessage mimeMessage = mailSender.createMimeMessage()
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false)
-        helper.setTo(unmanagedEmail.to)
-        helper.setSubject(unmanagedEmail.subject)
-        helper.setText(unmanagedEmail.text, false)
-        helper.setFrom(System.getenv("GMAIL_FROM"))
-        mailSender.send(mimeMessage)
+        Email from = new Email(System.getenv("GMAIL_USERNAME"))
+        String subject = unmanagedEmail.subject
+        Email to = new Email(unmanagedEmail.to)
+        Content content = new Content("text/plain", unmanagedEmail.text)
+        Mail mail = new Mail(from, subject, to, content)
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"))
+        Request request = new Request()
+        request.method = Method.POST
+        request.endpoint = "mail/send"
+        request.body = mail.build()
+        Response response = sg.api(request)
+        System.out.println(response.statusCode)
+        System.out.println(response.body)
+        System.out.println(response.headers)
     }
 
 }
