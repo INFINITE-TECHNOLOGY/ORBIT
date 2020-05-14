eb68ad3183d3cef667c8a713d7cebadf7e44c368
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-14 09:57:55 +0300

    Progress

--- orbit-sdk/src/main/groovy/io/infinite/orbit/services/HistoryService.groovy
+++ orbit-sdk/src/main/groovy/io/infinite/orbit/services/HistoryService.groovy
@@ -11,7 +11,6 @@ import io.infinite.orbit.repositories.ReconciliationRecordRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 
-import javax.annotation.PostConstruct
 import java.time.LocalDate
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -29,22 +28,21 @@ class HistoryService extends CrmServiceBase {
 
     XmlSlurper xmlSlurper = new XmlSlurper()
 
-    @PostConstruct
     void downloadHistory() {
         Optional<Date> dateFrom = reconciliationRecordRepository.lastDownloadDate()
-        Integer page = 1
+        Integer offset = 1
         while (true) {
             HttpResponse httpResponse = crmRequest("""<request point="315">
     <reconciliation 
     begin="${dateFrom.present ? dateFormatter.format(dateFrom.get().toInstant()) : "2020-01-01T00:00:00+0300"}" 
     end="${dateFormatter.format(ZonedDateTime.now())}" 
     payments="1" 
-    offset="$page"/>
+    offset="${offset * 1000}"/>
 </request>""")
             def response = xmlSlurper.parseText(httpResponse.body)
             if (response.result.@count.toInteger() > 0) {
                 reconciliationRecordRepository.saveAll(response.result.payment.collect { convert(it) })
-                page++
+                offset++
             } else {
                 break
             }
@@ -73,7 +71,7 @@ class HistoryService extends CrmServiceBase {
     }
 
     Set<HistoryRecord> getHistory(String userGuid, Optional<String> tranCount) {
-
+        downloadHistory()
         return []
     }
 
