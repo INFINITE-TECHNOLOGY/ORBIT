552ab71658b0db1f2b043d588065e06cc9d588b4
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-15 05:28:09 +0300

    Progress

--- orbit-sdk/src/main/groovy/io/infinite/orbit/services/HistoryService.groovy
+++ orbit-sdk/src/main/groovy/io/infinite/orbit/services/HistoryService.groovy
@@ -57,8 +57,8 @@ class HistoryService extends CrmServiceBase {
     ReconciliationRecord convertToReconciliationRecord(def xmlRecord) {
         return new ReconciliationRecord(
                 crmId: xmlRecord.@id,
-                dateLocal: fastDateFormat.parse(xmlRecord.@dateLocal.toString()),
-                dateUtc: fastDateFormat.parse(xmlRecord.@dateLocal.toString()).toInstant().toDate(),
+                dateLocal: fastDateFormat.parse(xmlRecord.@date.toString()),
+                dateUtc: fastDateFormat.parse(xmlRecord.@date.toString()).toInstant().toDate(),
                 state: xmlRecord.@state,
                 substate: xmlRecord.@substate,
                 code: xmlRecord.@code,
